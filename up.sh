a6a4bc1f242a0ce7399784f0fd8be261d6eacf88
kojo1984 <velimir.kojic@hotmail.com>
2021-09-20 14:26:31 +0200

    commit on main

--- up.sh
+++ up.sh
@@ -3,5 +3,5 @@ export DPATH=$(pwd)
 export SERVICE=monitoring
 export UID=$(id -u)
 export GID=$(id -g)
-chmod -R $UID $SERVICE
+chown -R $UID $SERVICE
 docker-compose -f "$DPATH/$SERVICE/docker-compose.yml" up -d --build
