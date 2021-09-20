435639a4d025977fee958a2a49a4434609dce6f2
kojo1984 <velimir.kojic@hotmail.com>
2021-09-20 13:17:42 +0200

    commit on main

--- up.sh
+++ up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export DPATH=$(pwd)
+export SERVICE=prometheus
 export UID=$(id -u)
 export GID=$(id -g)
-docker-compose -f "$DPATH/docker-compose.yml" up -d --build
+docker-compose -f "$DPATH/$SERVICE/docker-compose.yml" up -d --build
