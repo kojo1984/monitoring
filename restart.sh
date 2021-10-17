92188a29851a00fd192031ebe62bc3c8f0d88c08
kojo1984 <velimir.kojic@hotmail.com>
2021-10-17 22:02:25 +0200

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export DPATH=/media/velimirkojic/m2/docker/srv/docker
+export DPATH=$(pwd)
 export UID=$(id -u)
 export GID=$(id -g)
-docker-compose -f "$DPATH/monitoring/docker-compose.yml" down
-docker-compose -f "$DPATH/monitoring/docker-compose.yml" up -d --build
+docker-compose -f "$DPATH/prometheus/docker-compose.yml" down
+docker-compose -f "$DPATH/prometheus/docker-compose.yml" up -d --build
